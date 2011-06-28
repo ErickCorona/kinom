61d5d54e060cb5400c601ec30f439516b4e29aef
jsus.159 <jsus.159@gmail.com>
2011-06-28 02:47:20 +0000

    Frame Venta

--- Kinom/src/gui/FrmVentaTicket.java
+++ Kinom/src/gui/FrmVentaTicket.java
@@ -36,6 +36,7 @@ public class FrmVentaTicket extends JFrame {
 	 * Create the frame.
 	 */
 	public FrmVentaTicket() {
+		setTitle("Venta de Tickets");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 931, 651);
 		contentPane = new JPanel();
