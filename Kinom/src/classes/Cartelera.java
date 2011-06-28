77f548efd11477cb9567800b5afe4c9d6325c8b5
beto.quartz <beto.quartz@gmail.com>
2011-06-28 23:51:54 +0000

    Update Cartelera + JavaDoc

--- Kinom/src/classes/Cartelera.java
+++ Kinom/src/classes/Cartelera.java
@@ -22,16 +22,26 @@ public class Cartelera {
 	private ArrayList<Funcion> funciones;
 	private Calendar currentDate;
 	
+	/**
+	 * Crea una instancia de Cartelera asignando como fecha actual <i>currentDate</i> la del momento en que se crea. Usa esa fecha para cargar las funciones de la base de datos. 
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 * @throws ParseException
+	 */
 	public Cartelera() throws SQLException, ClassNotFoundException, ParseException{
 		currentDate = Calendar.getInstance();
 		currentDate.set(Calendar.DATE, currentDate.get(Calendar.DATE)-1);
 		loadFunciones(Calendar.getInstance());
 	}
-
-	public Cartelera(ArrayList<Funcion> funciones) {
-		this.funciones = funciones;
-	}
 	
+	/**
+	 * Toma las películas del <i>ArrayList</i> global <i>funciones</i> a proyectarse en una fecha específica en diferentes salas.
+	 * @param fecha <i>Calendar</i> que contiene la fecha deseada.
+	 * @return <i>ArrayList</i> de <i>Funcion</i> que contiene las diferentes películas.
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 * @throws ParseException
+	 */
 	public ArrayList<Funcion> getPeliculas(Calendar fecha) throws SQLException, ClassNotFoundException, ParseException{
 		loadFunciones(fecha);
 		ArrayList<Funcion> p = new ArrayList<Funcion>();
@@ -53,19 +63,33 @@ public class Cartelera {
 		return p;
 	}
 	
-	public ArrayList<Funcion> getFunciones(Pelicula pel) throws SQLException, ClassNotFoundException, ParseException{
-		Calendar fecha = currentDate;
-		loadFunciones(fecha);
+	/**
+	 * Toma las funciones que serán proyectadas en la fecha actual <i>currentDate</i> de una película en específico en una sala.
+	 * @param pel Película de la que se quieren las funciones.
+	 * @param sala Sala en la que está la película.
+	 * @return <i>ArrayList</i> de <i>Funcion</i> que contiene las diferentes funciones.
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 * @throws ParseException
+	 */
+	public ArrayList<Funcion> getFunciones(Pelicula pel, int sala) throws SQLException, ClassNotFoundException, ParseException{
 		ArrayList<Funcion> p = new ArrayList<Funcion>();
-		boolean isToday = fecha.get(Calendar.DAY_OF_YEAR)==Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
+		boolean isToday = currentDate.get(Calendar.DAY_OF_YEAR)==Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
 		for(Funcion f:funciones){
-			if(f.getPelicula().equals(pel) 
-				&& (!isToday || f.getHorario().getTimeInMillis()>=fecha.getTimeInMillis()-7200000))
+			if(f.getPelicula().equals(pel) && f.getSala().getNumero()==sala
+				&& (!isToday || f.getHorario().getTimeInMillis()>=currentDate.getTimeInMillis()-7200000))
 				p.add(f);
 		}
 		return p;
 	}
 	
+	/**
+	 * Carga en el <i>ArrayList</i> global <i>funciones</i> desde la base de datos las funciones de una fecha especificada.
+	 * @param date Fecha a consultar.
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 * @throws ParseException
+	 */
 	public void loadFunciones(Calendar date) throws SQLException, ClassNotFoundException, ParseException{
 		if(currentDate.get(Calendar.DAY_OF_YEAR)==date.get(Calendar.DAY_OF_YEAR))
 			return;
