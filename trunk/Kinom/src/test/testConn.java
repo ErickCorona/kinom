36e8411456dd4df88e6b4d1cc07ea071c3934582
jsus.159@gmail.com <jsus.159@gmail.com@3af9866d-ae14-bd0a-60a1-596a0b31018e>
2011-07-15 09:10:08 +0000

    Mal Corte del dia

--- trunk/Kinom/src/test/testConn.java
+++ trunk/Kinom/src/test/testConn.java
@@ -2,32 +2,28 @@ package test;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 import bd.Conexion;
-import classes.Funcion;
 
 public class testConn {
 	public static void main(String args[]){
-		ArrayList<Funcion> funciones = new ArrayList<Funcion>();
-		int sala = 1;
-		int pel = 1;
 		Conexion conn = new Conexion();
-		String stm = "SELECT * FROM funciones,peliculas,salas WHERE funciones.id_pel=peliculas.id_pel AND funciones.id_sala=salas.id_sala AND salas.id_sala = "+sala+" AND peliculas.id_pel = "+pel+" AND strftime('%s',funciones.hro_fun) > strftime('%s',date('now','localtime'))";
-		//String stm = "SELECT * FROM funciones";
+		/*String stm = "SELECT DISTINCT " +
+				"peliculas.id_pel as id_pel,peliculas.nom_pel as nom_pel " +
+				"FROM peliculas,funciones,salas " + 
+				"WHERE funciones.id_sala = salas.id_sala " +
+				"AND peliculas.id_pel = funciones.id_pel " +
+				"AND date(funciones.hro_fun) = date('now', 'localtime') " +
+				"AND salas.id_sala = 2" ;//+ id_pel;*/
+		String stm = "SELECT peliculas.nom_pel,COUNT(ventas.id_ven) as venta,SUM(ventas.prec_ven) as total FROM  ventas,funciones,peliculas  WHERE  ventas.id_fun = funciones.id_fun  AND funciones.id_pel = peliculas.id_pel  AND date(funciones.hro_fun)=date('now','localtime')  AND funciones.id_pel = 1 AND funciones.id_sala = 3";
+		//String stm = "SELECT DISTINCT peliculas.id_pel as id_pel,peliculas.nom_pel as nom_pel FROM peliculas LEFT JOIN funciones LEFT JOIN salas WHERE funciones.id_sala = salas.id_sala AND peliculas.id_pel = funciones.id_pel AND date(funciones.hro_fun) = date('now', 'localtime') AND salas.id_sala = 2";
 		System.out.println(stm);
-		ResultSet rs;
+		ResultSet rs ;
 		try {
 			rs = conn.executeQ(stm);
-			while(rs.next()){
-				System.out.println("id:" + rs.getString(1));
-				System.out.println(""+rs.getString(3));
-				System.out.println(""+rs.getString(4));
-				System.out.println(""+rs.getString(5));
-				System.out.println(""+rs.getString(6));
-				System.out.println(""+rs.getString(7));
+					while(rs.next()){
+				System.out.println("Pelicula:"+rs.getString("nom_pel"));
 			}
-			conn.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -35,7 +31,7 @@ public class testConn {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		
 	}
 
